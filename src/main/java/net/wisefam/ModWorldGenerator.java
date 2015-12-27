ef98ca172e987b3be58b42da77fc47740fdaf882
Kevin Wise <kevin@wisefam.net>
2015-12-27 13:19:34 -0700

    Changed material characteristics and generation parameters

--- src/main/java/net/wisefam/ModWorldGenerator.java
+++ src/main/java/net/wisefam/ModWorldGenerator.java
@@ -2,7 +2,6 @@ package net.wisefam;
 
 import com.google.common.base.Predicate;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.block.state.pattern.BlockHelper;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
@@ -20,7 +19,7 @@ public class ModWorldGenerator implements IWorldGenerator {
     private WorldGenMinable titaniumOreGenerator; //Generates Titanium Ore (used in Overworld)
 
     public ModWorldGenerator() {
-        this.titaniumOreGenerator = new WorldGenMinable(ModBlocks.titaniumOre.getDefaultState(), 8, new UndergroundBlockPredicate());
+        this.titaniumOreGenerator = new WorldGenMinable(ModBlocks.titaniumOre.getDefaultState(), 7, new UndergroundBlockPredicate());
         GameRegistry.registerWorldGenerator(this, 0);
     }
 
@@ -28,7 +27,7 @@ public class ModWorldGenerator implements IWorldGenerator {
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
         switch (world.provider.getDimensionId()) {
             case 0: //Overworld
-                runGenerator(this.titaniumOreGenerator, world, random, chunkX, chunkZ, 20, 20, 40);
+                runGenerator(this.titaniumOreGenerator, world, random, chunkX, chunkZ, 2, 40, 50);
                 break;
             case -1: //Nether
 
@@ -44,7 +43,7 @@ public class ModWorldGenerator implements IWorldGenerator {
             throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
 
         int heightDiff = maxHeight - minHeight + 1;
-        for (int i = 0; i < chancesToSpawn; i ++) {
+        for (int i = 0; i < chancesToSpawn; i++) {
             int x = chunk_X * 16 + rand.nextInt(16);
             int y = minHeight + rand.nextInt(heightDiff);
             int z = chunk_Z * 16 + rand.nextInt(16);
@@ -57,15 +56,15 @@ public class ModWorldGenerator implements IWorldGenerator {
         public boolean apply(IBlockState input) {
             return input != null && (
                     input.getBlock() == Blocks.stone || // includes Granite, Polished Granite, Diorite, Polished Diorite, Andesite, and Polished Andesite
-                    input.getBlock() == Blocks.cobblestone ||
-                    input.getBlock() == Blocks.dirt ||
-                    input.getBlock() == Blocks.gravel ||
-                    input.getBlock() == Blocks.lava ||
-                    input.getBlock() == Blocks.water ||
-                    input.getBlock() == Blocks.sand ||
-                    input.getBlock() == Blocks.sandstone ||
-                    input.getBlock() == Blocks.flowing_lava ||
-                    input.getBlock() == Blocks.flowing_water
+                            input.getBlock() == Blocks.cobblestone ||
+                            input.getBlock() == Blocks.dirt ||
+                            input.getBlock() == Blocks.gravel ||
+                            input.getBlock() == Blocks.lava ||
+                            input.getBlock() == Blocks.water ||
+                            input.getBlock() == Blocks.sand ||
+                            input.getBlock() == Blocks.sandstone ||
+                            input.getBlock() == Blocks.flowing_lava ||
+                            input.getBlock() == Blocks.flowing_water
             );
         }
     }
--- src/main/java/net/wisefam/materials/TitaniumMaterial.java
+++ src/main/java/net/wisefam/materials/TitaniumMaterial.java
@@ -10,8 +10,9 @@ import net.minecraftforge.common.util.EnumHelper;
 
 public class TitaniumMaterial extends Material {
 
-    public static final Item.ToolMaterial TOOL = EnumHelper.addToolMaterial("titanium", 3, 1561, 6.0F, 3.0F, 30);
-    public static final ItemArmor.ArmorMaterial ARMOR = EnumHelper.addArmorMaterial("titanium", "titanium", 33, new int[]{3, 8, 6, 3}, 30);
+    // TODO: when updated to 1.9, change damage to 2.0f and lower the reload time
+    public static final Item.ToolMaterial TOOL = EnumHelper.addToolMaterial("titanium", 2, 50, 6.0F, 2.0F, 30);
+    public static final ItemArmor.ArmorMaterial ARMOR = EnumHelper.addArmorMaterial("titanium", "titanium", 7, new int[]{2, 6, 5, 2}, 30);
 
     public TitaniumMaterial() {
         super(MapColor.ironColor);
